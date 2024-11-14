e6f260ae1d58375a3b4ae9bbce65a1520cf96e11
whlsls3377 <whlsls3377@naver.com>
2024-11-14 16:56:48 +0900

    refactor : dto part, nickname add commit

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,7 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,8 @@ public class SignupRequest {
 
     @NotBlank @Email
     private String email;
+    @NotBlank @Size(min = 15)
+    private String nickname;
     @NotBlank
     private String password;
     @NotBlank
